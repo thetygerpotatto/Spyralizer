c832a005368a756465a51ffb74b066748acf1d31
thetygerpotatto <letras.cj@gmail.com>
2024-03-07 09:21:49 -0500

    Implemented a new abstraction to represent pendulums and manage them(Not functional yet)

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,18 @@
 #include <cmath>
 #include <complex>
+#include <cstddef>
 #include <iostream>
 #include <raylib.h>
 #include <raymath.h>
+#include "pendulum_vec.hpp"
+#include "pendulum.hpp"
 
 
 int main() {
     const int screen_widht = 1080;
     const int screen_height = 720;
     Vector2 offset = {screen_widht/2.0, screen_height/2.0};
-
-    float tita = 0.0f;
-    float tita2 = 3*PI/2.0f;
-    float norm = 100.0f;
-    float norm2 = 200.0f;
-
-    Vector2 origin = {0, 0};
-    Vector2 CompPoint = {0};
-    Vector2 SeccondCompPoint = {0};
+    Vector2 origin = {0};
 
     Camera2D camera {0};
     camera.offset = offset;
@@ -25,24 +20,30 @@ int main() {
     camera.zoom = 1.0f;
     camera.rotation = 0.0f;
     
+    pendulum_vec penvec;
+
+    pendulum pen(origin, (Vector2){100, -100});
+    pendulum pen2(pen, 270.0f, 77.0f);
+
+    pen.SetRotationRate(PI/64);
+    pen2.SetRotationRate(-PI/128);
+
+    penvec.push_back(pen);
+    penvec.push_back(pen2);
+    
+    
 
     InitWindow(screen_widht, screen_height, "Spyralizer");
     SetTargetFPS(120); 
 
     while (!WindowShouldClose()) {
-
-        CompPoint = {norm*cosf(tita), norm*sinf(tita)};
-        SeccondCompPoint = {CompPoint.x + norm2*cosf(tita2), CompPoint.y + norm2*sinf(tita2)};
-
-        tita += PI/64.0f;
-        tita2 -= PI/180.0f;
-        
-        
+        penvec.rotate_pens();
         BeginDrawing();
         BeginMode2D(camera);
-        //ClearBackground({BLACK});
-        DrawLineV(origin, CompPoint, {0x4E, 0xD9, 0xB5, BLEND_ADD_COLORS});
-        DrawLineV(CompPoint, SeccondCompPoint,{0x4E, 0xD9, 0xB5, BLEND_ADD_COLORS});
+        ClearBackground({BLACK});
+        for (size_t i = 0; i < penvec.size(); ++i) {
+            DrawLineV(*penvec[i].base, *penvec[i].head, {0x4E, 0xD9, 0xB5, 0xFF});
+        }
         EndMode2D();
         EndDrawing();
     }
--- src/pendulum.hpp
+++ src/pendulum.hpp
@@ -3,10 +3,9 @@
 
 #include <cmath>
 #include <raylib.h>
+#include <iostream>
 
 class pendulum {
-    Vector2 *base;
-    Vector2 *head;
     bool is_origin = false;
     float angle = 0.0f;
     float magnitude = 0.0f;
@@ -14,9 +13,38 @@ class pendulum {
     pendulum* father = nullptr;
 
 public:
-    pendulum(pendulum &fa) {
+    Vector2 *base;
+    Vector2 *head;
+
+    pendulum() {
+
+    }
+
+    pendulum(pendulum &p) {
+        is_origin = p.get_origin_status();
+        angle = p.get_angle();
+        magnitude = p.get_magnitude();
+        rotation_rate = p.get_rotation_rate();
+        father = p.get_father();
+        
+        if(is_origin) {
+            base = new Vector2;
+            *base = *p.base;
+        } else {
+            base = father->head;
+        }
+        head = new Vector2;
+        *head = *p.head;
+    }
+
+    pendulum(pendulum &fa, float x, float y) {
         father = &fa;  
         base = father->head;
+        head = new Vector2;
+        head->x = x;
+        head->y =y;
+        magnitude = sqrtf(powf(head->x - base->x, 2)+ powf(head->y - base->y, 2));
+        angle = asinf((head->x - base->x) / magnitude);
     }
 
     pendulum(Vector2 b, Vector2 h) {
@@ -26,8 +54,8 @@ public:
         *head = h;
         is_origin = true;
 
-        magnitude = sqrtf(powf((h.x - b.x),2) + pow((h.y - b.y), 2));
-        angle = acosf(h.x - b.x / magnitude);
+        magnitude = sqrtf(powf((h.x - b.x),2) + powf((h.y - b.y), 2));
+        angle = acosf((h.x - b.x) / magnitude);
     }
 
     ~pendulum(){
@@ -37,7 +65,55 @@ public:
         }
     }
 
-    
+    void SetRotationRate(float r) {
+        rotation_rate = r;
+    }
+
+
+
+    void Rotate() {
+        angle += rotation_rate;
+        head->x = cosf(angle)*magnitude + base->x;
+        head->y = sinf(angle)*magnitude + base->y;
+    }
+
+    bool get_origin_status() {
+        return is_origin;
+    }
+
+    float get_angle() {
+        return angle;
+    }
+
+    float get_magnitude() {
+        return magnitude;
+    }
+
+    float get_rotation_rate() {
+        return rotation_rate;
+    }
+
+    pendulum* get_father() {
+        return father;
+    }
+
+    pendulum& operator=(pendulum p) {
+        is_origin = p.get_origin_status();
+        angle = p.get_angle();
+        magnitude = p.get_magnitude();
+        rotation_rate = p.get_rotation_rate();
+        father = p.get_father();
+        
+        if(is_origin) {
+            base = new Vector2;
+            *base = *p.base;
+        } else {
+            base = father->head;
+        }
+        head = new Vector2;
+        *head = *p.head;
+        return *this;
+    }
 
 };
 
